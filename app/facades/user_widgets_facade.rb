78e891ad9853544b206d49c35f58ea5e98e8a171
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 13:22:04 -0700

    Add method to grab user name from API response. Add method to grab user avatar url from API response.

--- app/facades/user_widgets_facade.rb
+++ app/facades/user_widgets_facade.rb
@@ -6,16 +6,24 @@ class UserWidgetsFacade
     @user_id = user_id
   end
 
+  def created_by
+    user_widgets_data[:data][:widgets][0][:user][:name]
+  end
+
+  def avatar
+    user_widgets_data[:data][:widgets][0][:user][:images][:small_url]
+  end
+
   def visible
     user_widgets_data.map do |widget|
-      Widget.new(data)
+      Widget.new(widget)
     end
   end
 
   private
 
   def widget_client
-    @widget_client = WidgetClient.new
+    @widget_client ||= WidgetClient.new
   end
 
   def user_widgets_data
