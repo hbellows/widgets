14c6941babf5839f71efe31a5f1e8f984a2a62a9
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 13:22:45 -0700

    Refactor PORO to remove avatar attribute since this will only be used on the show page

--- app/services/widget.rb
+++ app/services/widget.rb
@@ -3,7 +3,6 @@ class Widget
   attr_reader :name, 
               :description,
               :user_id,
-              :user_avatar,
               :created_by
 
   def initialize(data)
@@ -11,7 +10,6 @@ class Widget
     @name = data[:name]
     @description = data[:description]
     @user_id = data[:user][:id]
-    @user_avatar = data[:user][:images][:small_url]
     @created_by = data[:user][:name]
   end
 end
