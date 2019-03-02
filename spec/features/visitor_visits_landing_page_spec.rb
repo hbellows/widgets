965c2ade9f45969100c80cc20e8dd3397602948a
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 08:14:17 -0700

    Add spec for visitor visiting landing page to see visible widgets

---
+++ spec/features/visitor_visits_landing_page_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Landing Page' do
+  context 'A guest visits the landing page and' do
+    it 'displays all visible widgets and the name of the widget\'s creator' do
+      visit '/widgets'
+
+      expect(page).to have_content('Widgets')
+      expect(page).to have_css('.widget', count: 12)
+      within(first('.widget')) do
+        expect(page).to have_content('Widget Name')
+        expect(page).to have_link('User Name')
+      end
+    end
+  end
+end
