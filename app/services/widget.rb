d1fcff772e6435242958b34d18eddb34c594ca43
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 12:58:30 -0700

    Refactor widget PORO to collect user avatar for display on the show page

--- app/services/widget.rb
+++ app/services/widget.rb
@@ -3,6 +3,7 @@ class Widget
   attr_reader :name, 
               :description,
               :user_id,
+              :user_avatar,
               :created_by
 
   def initialize(data)
@@ -10,6 +11,7 @@ class Widget
     @name = data[:name]
     @description = data[:description]
     @user_id = data[:user][:id]
+    @user_avatar = data[:user][:images][:small_url]
     @created_by = data[:user][:name]
   end
 end
