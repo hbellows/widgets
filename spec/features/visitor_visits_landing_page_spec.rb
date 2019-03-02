991397ddaede38d06ed4fb3a41d710c349dc3ffa
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 09:00:02 -0700

    Add vcr to test to stub out API data in order to reduce burden on the client

--- spec/features/visitor_visits_landing_page_spec.rb
+++ spec/features/visitor_visits_landing_page_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 describe 'Landing Page' do
   context 'A guest visits the landing page and' do
     it 'displays all visible widgets and the name of the widget\'s creator' do
-      visit '/widgets'
+      VCR.use_cassette('Landing Page') do
+        visit '/widgets'
 
-      expect(page).to have_content('Widgets')
-      expect(page).to have_css('.widget', count: 12)
-      within(first('.widget')) do
-        expect(page).to have_content('Widget Name')
-        expect(page).to have_link('User Name')
+        expect(page).to have_content('Widgets')
+        expect(page).to have_css('.widget', count: 20)
+        within(first('.widget')) do
+          expect(page).to have_content('Widget Name')
+          expect(page).to have_link('User Name')
+        end
       end
     end
   end
