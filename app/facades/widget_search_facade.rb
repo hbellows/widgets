1af2320b6a42c26cb4f06780f3f97360b6265e00
Harper Bellows <harper.bellows@gmail.com>
2019-03-02 20:31:14 -0700

    Add a search facade to handle parsing returned API data

---
+++ app/facades/widget_search_facade.rb
@@ -0,0 +1,32 @@
+class WidgetSearchFacade
+
+  attr_reader :keyword
+
+  def initialize(keyword)
+    @keyword = keyword
+  end
+
+  def visible
+    widget_search_data.map do |widget|
+      Widget.new(widget)
+    end
+  end
+
+  def count
+    if widget_search_data.empty?
+      "No widgets matching '#{keyword}'"
+    else
+      "#{widget_search_data.count} widgets found"
+    end
+  end
+
+  private
+
+  def showoff_client
+    @showoff_client ||= ShowoffClient.new
+  end
+
+  def widget_search_data
+    showoff_client.search_widgets(keyword)[:data][:widgets]
+  end
+end
