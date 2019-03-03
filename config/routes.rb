72d785db8896042283ee5d45589e8a7e6bb4148e
Harper Bellows <harper.bellows@gmail.com>
2019-03-03 15:57:32 -0700

    Remove search bar from widget index view.  Update root path to widgets index. Add basic styling.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  get '/search', to: 'search#index'
+  get '/search', to: 'widgets#index'
   resources :widgets, only: %i[index show]
 end
