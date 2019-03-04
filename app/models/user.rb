b911e9cde092d85b1173b84f345e91e87b11c567
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 09:57:52 -0700

    Add user model and passing validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :first_name, :last_name, :email,:password, :role, presence: true
+  validates :password_confirmation,  presence: true, on: :create       
+  validates :email, uniqueness: true
+
+  enum role: %i[user admin]
 end
