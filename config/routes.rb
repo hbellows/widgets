00da600581fcf44b9ae82b1af08af22aafbf41f3
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 11:33:11 -0700

    Add route to users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'widgets#index'
+  root 'welcome#index'
 
   get '/search', to: 'search#index'
   resources :widgets, only: %i[index show]
+  resources :users, only: %i[new]
 end
