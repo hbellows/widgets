7ab020540e7bac76397db0eb3159af26a56ad8cb
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 12:19:25 -0700

    Add feature to register a guest. Add passing happy and sad path tests

--- spec/features/guest_can_register_spec.rb
+++ spec/features/guest_can_register_spec.rb
@@ -7,7 +7,6 @@ context 'Guest Registration' do
       click_on 'Sign Up'
 
       expect(current_path).to eq(new_user_path)
-      # save_and_open_page
 
       fill_in :user_first_name,	with: 'Mike' 
       fill_in :user_last_name,	with: 'Heft'
@@ -17,6 +16,35 @@ context 'Guest Registration' do
       click_on 'Create Account'
 
       expect(current_path).to eq(dashboard_path)
+
+      user = User.first 
+ 
+      expect(user.id).to eq(1)
+      expect(user.first_name).to eq('Mike')
+      expect(user.last_name).to eq('Heft')
+      expect(user.email).to eq('mike@heft.com')
+      expect(user.password_digest.length).to eq(60)
+      expect(user.role).to eq('user')
+    end
+  end
+
+  describe 'Guest provides incomplete information' do
+    it 'displays an error message and does not register the guest' do
+      visit '/widgets'
+      click_on 'Sign Up'
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in :user_first_name,	with: 'Mike' 
+      fill_in :user_last_name,	with: ''
+      fill_in :user_email,	with: 'mike@heft.com'
+      fill_in :user_password,	with: 'hamburger1'
+      fill_in :user_password_confirmation,	with: 'hamburger1' 
+      click_on 'Create Account'
+
+      expect(current_path).to eq(users_path)
+
+      expect(page).to have_content('Create A Widgets Account')
     end
   end
 end
