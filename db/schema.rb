78f20a36c69cd1d73dcf73bbe7b83f7899918ce5
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 06:09:58 -0700

    Add a few users to the seeds and db to test out login and interacting with API

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_03_04_125850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "token"
+    t.string "refresh"
+    t.integer "role", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create!(first_name: 'Boss', last_name: 'Baby', email: 'boss@me.com',
+                     password: 'imabossbaby', password_confirmation: 'imabossbaby',
+                     token: '4355f094978ce1c7dbc074a8a07056b402b60f13adbb39f2893becd9dc256e9d',
+                     refresh: '301c9c023c798d38a0c97067b8164b9cd0eb2e91827e9e28ad8e4208c35f4453', 
+                     role: 1)
+
+user_1 = User.create!(first_name: 'Justa', last_name: 'User', email: 'user@me.com',
+                      password: 'imjustauser', password_confirmation: 'imjustauser',
+                      token: '4ff34069ded782a13d7e9685d4c25f86b0393b2c68f2a8f0204993557f78a204', 
+                      refresh: '7ff7f9aa4a56830b1044c5a173a9ae35a855daf24a98c9392e4e8a842bcc0d8f' 
+                      )
+
+user_2 = User.create!(first_name: 'Just', last_name: 'Anotheruser', email: 'user2@me.com',
+                      password: 'anotheruser', password_confirmation: 'anotheruser',
+                      token: '4a03a329ebdc460be35d452132e9fa671279da9e236bf1b115beb1815baf3996', 
+                      refresh: '70399b9ad64130d9e85aab6f4e96144225330113704a62c4ea1afbe1262dca2b'
+                      )
