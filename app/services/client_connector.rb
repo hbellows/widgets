a597875d7d18f857869678951ace347a33909102
Harper Bellows <harper.bellows@gmail.com>
2019-03-05 14:01:42 -0700

    Add clean up after client refactor

--- app/services/client_connector.rb
+++ app/services/client_connector.rb
@@ -1,7 +1,11 @@
 module ClientConnector
   private
 
-  def showoff_client
-    @showoff_client ||= ShowoffClient.new
+  def showoff_widget_client
+    @showoff_widget_client ||= ShowoffWidgetClient.new
+  end
+
+  def showoff_user_client
+    @showoff_user_client ||= ShowoffUserClient.new(user_token)
   end
 end
