524418ea406d90c0a1b25ff3bb317b77c45ee4f4
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 17:42:45 -0700

    Add controller logic creating and destroying a session and for validating provided credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+  helper_method :current_user, :require_user
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def require_user
+    unless current_user
+      flash[:error] = 'You must be signed in for that action'
+      redirect_to root_path
+    end
+  end
 end
