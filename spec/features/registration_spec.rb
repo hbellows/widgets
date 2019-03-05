9833f49d493a54381a9d0aa845e4e819719880e6
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 17:39:53 -0700

    Rename spec files to be more readable and to include a wider range tests in each file

---
+++ spec/features/registration_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper' 
+
+context 'Guest Registration' do
+  describe 'Guest provides all required information' do
+    it 'registers the guest' do
+      visit '/widgets'
+      click_on 'Sign Up'
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in :user_first_name,	with: 'Mike' 
+      fill_in :user_last_name,	with: 'Heft'
+      fill_in :user_email,	with: 'mike@heft.com'
+      fill_in :user_password,	with: 'hamburger1'
+      fill_in :user_password_confirmation,	with: 'hamburger1' 
+      click_on 'Create Account'
+
+      expect(current_path).to eq(dashboard_path)
+
+      user = User.first 
+ 
+      expect(user.id).to eq(1)
+      expect(user.first_name).to eq('Mike')
+      expect(user.last_name).to eq('Heft')
+      expect(user.email).to eq('mike@heft.com')
+      expect(user.password_digest.length).to eq(60)
+      expect(user.role).to eq('user')
+    end
+  end
+
+  describe 'Guest provides incomplete information' do
+    it 'displays an error message and does not register the guest' do
+      visit '/widgets'
+      click_on 'Sign Up'
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in :user_first_name,	with: 'Mike' 
+      fill_in :user_last_name,	with: ''
+      fill_in :user_email,	with: 'mike@heft.com'
+      fill_in :user_password,	with: 'hamburger1'
+      fill_in :user_password_confirmation,	with: 'hamburger1' 
+      click_on 'Create Account'
+
+      expect(current_path).to eq(users_path)
+      expect(page).to have_content("Last name can't be blank")
+      expect(page).to have_content('Create New Widgets Account')
+    end
+  end
+end
