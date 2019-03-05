d44de3f16e4d9d4b6cb88355f9741633d96480f6
Harper Bellows <harper.bellows@gmail.com>
2019-03-04 17:45:36 -0700

    Add refactor to user views to include a welcome message for logged in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :require_user, :user, only: :show
+  
   def new
     @user = User.new
   end
@@ -22,5 +23,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email,
                                  :password, :password_confirmation)
+  end
+
+  def user
+    @user ||= User.find(current_user.id)
   end
 end
