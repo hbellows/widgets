48a74fca052b44cfe95f95eb0fe83a025d897cd6
Harper Bellows <harper.bellows@gmail.com>
2019-03-06 16:01:49 -0700

    Add skeleton for edit widget test

--- spec/features/users/users_widgets_spec.rb
+++ spec/features/users/users_widgets_spec.rb
@@ -95,4 +95,40 @@ context 'Signed in user can' do
       end
     end
   end
+
+  describe 'Edit a widget' do
+    it 'displays the newly created in the user\'s list of widgets' do
+      VCR.use_cassette('User Edit Widget Page') do
+
+        user = User.create!(first_name: 'Me', last_name: 'Myself', email: 'me@me.com',
+          password: 'hamburger1', password_confirmation: 'hamburger1',
+          token: 'aa2957980f434f5cf33e9b85f943ccdc83dfa5b6663bcbb1054ac206365893c0', 
+          refresh: 'e37fc760e82d4de58e82a50dbbd4c83e72e2580e4b1fd74fa6d3ade1007886b1' 
+          )
+        
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+        visit '/dashboard'
+        
+        within('')
+        click_on 'New Widget'
+
+        expect(current_path).to eq(my_widgets_path)
+
+
+        fill_in :user_widgets_name, with: 'Totally new widget'
+        fill_in :user_widgets_description, with: 'Super rad new widget'
+        choose('Visible')
+
+        click_on 'Create Widget'
+
+        expect(current_path).to eq(dashboard_path)
+
+        within('.my-visible-widgets') do
+          expect(page).to have_content('Totally new widget')
+          expect(page).to have_content('Super rad new widget')
+        end
+      end
+    end
+  end
 end
