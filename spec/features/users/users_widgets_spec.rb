4e74287a9528de132d393945f393a08cb0a7abe0
Harper Bellows <harper.bellows@gmail.com>
2019-03-07 10:36:59 -0700

    Attempt to add additional users in order to get better test results and to hopefully get CI build to pass

--- spec/features/users/users_widgets_spec.rb
+++ spec/features/users/users_widgets_spec.rb
@@ -65,10 +65,10 @@ context 'Signed in user can' do
     it 'displays the newly created in the user\'s list of widgets' do
       VCR.use_cassette('User Create Widget Page') do
 
-        user = User.create!(first_name: 'Me', last_name: 'Myself', email: 'me@me.com',
+        user = User.create!(first_name: 'Old Gregg', last_name: 'Baileys', email: 'me@cave.com',
           password: 'hamburger1', password_confirmation: 'hamburger1',
-          token: 'aa2957980f434f5cf33e9b85f943ccdc83dfa5b6663bcbb1054ac206365893c0', 
-          refresh: 'e37fc760e82d4de58e82a50dbbd4c83e72e2580e4b1fd74fa6d3ade1007886b1' 
+          token: '4536c1d91b36cd0471fe1c5ec736f803306289264b286803115d0307a89463b5', 
+          refresh: '361a9330c1917283bd4c149dbeb5e61d776f5d7646ae126e6186d62cd9a2df59' 
           )
         
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -88,7 +88,7 @@ context 'Signed in user can' do
 
         expect(current_path).to eq(dashboard_path)
 
-        within('.my-visible-widgets') do
+        within(first('#visible-widget')) do
           expect(page).to have_content('New Widgety Widget')
           expect(page).to have_content('So shiny')
         end
@@ -97,13 +97,13 @@ context 'Signed in user can' do
   end
 
   describe 'Edit a widget' do
-    it 'displays the newly created in the user\'s list of widgets' do
+    it 'displays the edited in the user\'s list of widgets' do
       VCR.use_cassette('User Edit Widget Page') do
 
-        user = User.create!(first_name: 'Me', last_name: 'Myself', email: 'me@me.com',
-          password: 'hamburger1', password_confirmation: 'hamburger1',
-          token: 'aa2957980f434f5cf33e9b85f943ccdc83dfa5b6663bcbb1054ac206365893c0', 
-          refresh: 'e37fc760e82d4de58e82a50dbbd4c83e72e2580e4b1fd74fa6d3ade1007886b1' 
+        user = User.create!(first_name: 'Justa', last_name: 'User', email: 'user@me.com',
+          password: 'imjustauser', password_confirmation: 'imjustauser',
+          token: '4ff34069ded782a13d7e9685d4c25f86b0393b2c68f2a8f0204993557f78a204', 
+          refresh: '7ff7f9aa4a56830b1044c5a173a9ae35a855daf24a98c9392e4e8a842bcc0d8f' 
           )
         
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -114,7 +114,7 @@ context 'Signed in user can' do
           click_on 'Edit'
         end
 
-        expect(current_path).to eq(edit_widget_path(121))
+        expect(current_path).to eq(edit_widget_path(134))
 
         fill_in :user_widgets_description, with: "I'm hiding"
         choose('Hidden')
@@ -123,13 +123,13 @@ context 'Signed in user can' do
 
         expect(current_path).to eq(dashboard_path)
 
-        within('.my-visible-widgets') do
-          expect(page).to_not have_content('Super Sweet Widget')
-          expect(page).to_not have_content('So so sweet')
+        within(first('#visible-widget')) do
+          expect(page).to_not have_content('New Hotness')
+          expect(page).to_not have_content('totes new newness')
         end
 
-        within('.my-hidden-widgets') do
-          expect(page).to have_content('Super Sweet Widget')
+        within(first('#hidden-widget')) do
+          expect(page).to have_content('New Hotness')
           expect(page).to have_content("I'm hiding")
         end
       end
